bf047dc2309c5a86e6569824a1b940a6671dba80
William Firmino <willian.firmino1@gmail.com>
2026-01-10 09:21:27 -0300

    End of section 3

---
+++ client/src/app/app.ts
@@ -0,0 +1,29 @@
+import {Component, inject, OnInit, signal} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {lastValueFrom} from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected members = signal<any>([])
+
+  async ngOnInit() {
+    this.members.set(await this.getMembers())
+  }
+
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+
+  }
+}
