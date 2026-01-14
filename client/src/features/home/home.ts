3ad0954a399f275d11ee0f48fec4082d78eba9d2
William Firmino <willian.firmino1@gmail.com>
2026-01-13 22:12:39 -0300

    End of section 5

---
+++ client/src/features/home/home.ts
@@ -0,0 +1,18 @@
+import {Component, signal} from '@angular/core';
+import {Register} from '../account/register/register';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    Register
+  ],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  protected registerMode = signal(false);
+
+  showRegister(value: boolean) {
+    this.registerMode.set(value);
+  }
+}
